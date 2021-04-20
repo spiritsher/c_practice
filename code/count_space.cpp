ebe26759d9d1d2db8cbaf412fa3798a01fe79282
spiritsher <jiawei.she@etas.com>
2021-04-20 23:32:18 +0200

    modified func for Multi-output

--- code/count_space.cpp
+++ code/count_space.cpp
@@ -6,64 +6,44 @@ sjw.spirit@gmail.com 20.04.2021
 
 #include <stdio.h>
 
-int count_char()
+int count_char(int* C_blanks, int* C_digital, int* C_char, int* C_letters, int* C_others)
 {
-    int blanks=0, digital = 0, totoal_chars =0, letters = 0, others =0;
+    // int blanks=0, digital = 0, totoal_chars =0, letters = 0, others =0;
     int c; // ust for int value of character
-    for (; (c=getchar())!= EOF; totoal_chars++)
+    for (; (c=getchar())!= EOF; *C_char++)
     {
-/*         switch (c)
-            {
-                case (c == ' '):
-                    ++blanks;
-                    break;
-    
-                case (c >= '0' && c <= '9'):
-                    ++digital;
-                    break;
-
-                case (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'z'):
-                    ++letters;
-                    break;
-
-                default:
-                    ++others;
-                    break;
-            } */
-
-        // wrong, swith case only fit for constant expression. 
-
         if (c == ' ')
         {
-            ++blanks;
+            // ++blanks;
+            ++*C_blanks;
         }
         else if (c >= '0' && c <= '9')
         {
-            ++digital;
+            // ++digital;
+            ++*C_digital;
         }
         else if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'z')
         {
-            ++letters;
+            // ++letters;
+            ++*C_letters;
         }
         else 
         {
-            ++others;
+            // ++others;
+            ++*C_others;
         }
-
     }
-    
+    return 0;
+}
+
+int main ()
+{
+    int blanks=0, digital = 0, totoal_chars =0, letters = 0, others =0;
+    count_char(&blanks, &digital, &totoal_chars, &letters, &others);
     printf ("count chars= %d,\n \
             count blanks= %d,\n \
             count digital= %d,\n \
             count letters= %d,\n \
             count others= %d,\n", 
-            totoal_chars,blanks, digital,letters, others);
-
-    return 0;
-
-}
-
-int main ()
-{
-    count_char();
+            totoal_chars, blanks, digital,letters, others);
 }
